e3e42eb42b919cc75c7ce6e79e6ef7b93a8990d1
26946-web <26946@bbw.ac.th>
2026-06-15 09:49:51 +0700

    Add files via upload
    วันนี้ได้เรียนรู้เรื่องการเขียนโปรแกรมการจำแนกเลขคู๋และเลขคี่ และได้รู้คำศํพท์เกี่ยวกับตัวเลขมากขึ้น

---
+++ 29lnwmuk_15-06-69.py
@@ -0,0 +1,8 @@
+print("โปนแกรม เลขคู่ หรือ เลขคี่ กันนะ!")
+number = int(input("Enter your number: "))
+
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+print(" โดย เจ้าตูดมุกโกะ 4/4")
